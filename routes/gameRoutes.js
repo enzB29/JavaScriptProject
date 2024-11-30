418908396aef55b17a7794d8cf12ac26778181d7
Corentin75 <corentindetournay05@gmail.com>
2024-11-30 22:26:45 +0100

    Ajouts de 300000 trucs environ

---
+++ routes/gameRoutes.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+
+const app = express();
+const prisma = new PrismaClient();
+
+
+
+app.get("/", async (req, res) => {
+    const games = await prisma.game.findMany();
+    res.render("games/index", {
+        games,
+    });
+});
+
+
+
+app.get('/new', async (req, res) => {
+    // to fetch genres and editors (useful for the form)
+    const genres = await prisma.genre.findMany(); 
+    const editors = await prisma.editor.findMany();
+
+    res.render("games/new", {
+        genres: genres,
+        editors: editors
+    });
+});
+
+
+
+app.post("/", async (req, res) => {
+    const { name, description, releaseDate, genreId, editorId } = req.body;
+    await prisma.game.create({
+        data: {
+            name,
+            description,
+            releaseDate: new Date(releaseDate),
+            genre: {
+                connect: { id: parseInt(genreId) }
+            },
+            editor: {
+                connect: { id: parseInt(editorId) }
+            }
+        }
+    });
+    res.redirect("/games");
+});
+
+
+
+app.post("/:id/delete", async (req, res) => {
+    const { id } = req.params;
+    await prisma.game.delete({
+        where: { id: parseInt(id) },
+    });
+    res.redirect('/games');
+});
+
+
+
+module.exports = app;
